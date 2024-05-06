32cd66fbca2d25fa461c388c3234f15c827e19d1
Smit Ambalia <ambaliasmit@gmail.com>
2024-05-06 22:42:40 +0530

    BullMQ based redis infra is up and running

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express, { Express } from "express";
 
-import serverConfig  from "./config/serverConfig";
+import serverConfig from "./config/serverConfig";
+import sampleProducerQueue from "./producers/sampleProducerQueue";
 import apiRouter from "./routes";
+import SampleWorker from "./workers/sampleWorker";
 
 const app: Express = express();
 
@@ -11,4 +13,12 @@ app.listen(serverConfig.PORT, () => {
   // const st : number = 1;
   console.log(`App is started on port : ${serverConfig.PORT}`);
   console.log("In the server");
+  SampleWorker('SampleQueue');
+
+  sampleProducerQueue("SampleJob", {
+    name: "Smit",
+    company: "Akhila Labs",
+    position: "Member of Technical staff",
+    location: "Remote | Ahmedabad | Jamjodhpur",
+  });
 });
